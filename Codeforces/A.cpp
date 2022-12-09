02621a3aa390b6405e071455b2094b22502fb66c
Sarthak Jha <jhasarthak123@gmail.com>
2022-12-09 12:47:10 +0530

    initial commit

---
+++ Codeforces/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <numeric>
+
+#define REP(i, a, b) for (int i = (a); i <= (b); i++)
+#define REP_Dec(i, a, b) for (int i = (a); i >= (b); i--)
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FOR1(i, n) for (int i = 1; i <= n; i++)
+#define print(n) cout << n << endl
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+using namespace std;
+
+typedef long long int ll;
+const int inf = 1e9;
+const ll N = 1'000'000'000'000L;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+    }
+    return 0;
+}

52b47d5d5304ee15948d3ecf7c55b6b2af04808b
SarthakJha-IITMandi <jhasarthak123@gmail.com>
2022-12-09 21:35:12 +0530

    added q5

--- Project Euler/5.py
+++ Project Euler/5.py
@@ -1,11 +1,1 @@
-n = 21
-while True:
-    check = 0
-    for i in range(1, 20):
-        if n%i!=0:
-            check = 1
-    if check == 1:
-        n = n + 1;
-    else:
-        print(n)
-        break
+print(5 * 7 * 9 * 11 * 13 * 16 * 17 * 19)
